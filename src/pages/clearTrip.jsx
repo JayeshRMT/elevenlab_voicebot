9fd13d7cac9e93d159888e23c507dde1c3c688f4
JayeshRMT <jayeshm7028@gmail.com>
2025-08-21 16:02:56 +0530

    update background image

--- src/pages/clearTrip.jsx
+++ src/pages/clearTrip.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import clearTripLogo from "../assests/clearTrip.png";
 import RMT from "../assests/rmt.png";
 import RadicalMinds from "../assests/radicalminds.png";
-import clearTripBackgroundUrl from "../assests/clearTripbg.jpg";
+import clearTripBackgroundUrl from "../assests/cleartripBg.jpg";
 
 const ClearTrip = () => {
   const [screenSize, setScreenSize] = useState("desktop");
