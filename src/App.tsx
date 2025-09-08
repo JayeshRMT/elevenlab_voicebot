6fd57409ac4b20fb34c5e8c2678ce7ea28d7233b
JayeshRMT <jayeshrm7028@gmail.com>
2025-09-08 14:00:35 +0530

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,26 @@ import BatikairNew from "./lastestVersion/batikair1NewVersion";
 import ClearTrip from "./pages/clearTrip";
 import TicketNetwork from "./pages/ticketnetwork";
 import Yamaha from "./pages/yamaha";
+import LoginSignup from "./pages/login";
 
 function App() {
-  // const OTAROUTE = "/travel/OTA/agent";
-  // const ENTERMENT = "/entertainment/events/agent";
-  // const AUTOMOBILE = "/automobile/bike/agent";
-
+  // const OTAROUTE = "/travel/OTA/agent"; // const ENTERMENT = "/entertainment/events/agent"; // const AUTOMOBILE = "/automobile/bike/agent";
   return (
     <Router>
       <Routes>
-        <Route path={`/`} element={<Navigate to={`/akasaair`} replace />} />
-        <Route path={`/akasaair`} element={<Aiagent1 />} />
-        <Route path={`/batikair`} element={<Batikair />} />
-        <Route path={`/yamaha`} element={<Yamaha />} />
+        <Route path="/" element={<LoginSignup />} />
 
-        <Route path={`/cleartrip`} element={<ClearTrip />} />
-        <Route path={`/ticketnetwork`} element={<TicketNetwork />} />
-        <Route path={`/akasaair-new`} element={<Aiagent1NewVersion />} />
-        <Route path={`/batikair-new`} element={<BatikairNew />} />
+        <Route path="/login" element={<LoginSignup />} />
+        <Route path="/akasaair/64567" element={<Aiagent1 />} />
+        <Route path="/batikair/64567" element={<Batikair />} />
+        <Route path="/yamaha" element={<Yamaha />} />
+        <Route path="/cleartrip/64567" element={<ClearTrip />} />
+        <Route path="/ticketnetwork" element={<TicketNetwork />} />
+        <Route path="/akasaair-new/64567" element={<Aiagent1NewVersion />} />
+        <Route path="/batikair-new/64567" element={<BatikairNew />} />
 
-        <Route path="*" element={<div>No Route Exist</div>} />
+        {/* Redirect all unknown routes to login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
